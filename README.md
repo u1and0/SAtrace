4b0d3f14786e3865e827ba8846085185b1b4ca8a
u1and0 <e01.ando60@gmail.com>
2020-07-25 16:58:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+satrace - SAtrace project's CLI tool
+
+# Usage:
+
+```sh
+# No option
+# Dump txt to SAtrace format data
+$ satrace *.txt
+2019-8-29 22:23:47  -35   -39.4   -55   ...
+2019-8-29 23:34:56  -31   -42.4   -43   ...
+
+# Electric Energy converter
+# X axis as line number
+$ satrace elen -f 425-575 *.txt
+
+# Electric Energy converter
+# X axis as point read from first line configure
+$ satrace elen -F 42-46 *.txt
+
+# Peak search
+$ satrace peak *.txt
+```
+
+
+## Subcommands
+* elen - Electric Energy converter
+* peak - Peak search method
+
+
+## Common options
+-d: Delimer (Default tab)
+
+```
+$ satrace -d , *.txt > traces.csv
+```
+
+
+## Electric Energy converter options
+-f: Filed range as point (multiple OK)
+-F: Filed range as  (multiple OK)
+
+```
+$ satrace elen -f 0-75 -f 205-280 -f 425-575 -f 725-800 -f 925-1000 *.txt
+```
+
+
+## Peak search options
+None
+
+
+# Data Structure
+```
+# 20200627_180505 *RST;*CLS;:INP:COUP DC;:BAND:RES 1 Hz;:AVER:COUNT 10;:SWE:POIN 1001;:FREQ:CENT 22.2 kHz;:FREQ:SPAN 2 kHz;:TRAC1:TYPE AVER;:INIT:CONT 0;:FORM REAL,32;:FORM:BORD SWAP;:INIT:IMM;:POW:ATT 0;:DISP:WIND:TRAC:Y:RLEV -30 dBm;
+   0     -93.21
+   1     -93.97
+   2     -94.93
+   3     -84.87
+   4     -96.31
+   5     -95.23
+   ...
+# <eof>
+```
+
+0  line   : configure strings
+1~ line   : data
+Last line : End of file
+
+0  column : points
+1~ column : data
+
+
+# Installation
+
+```
+$ go get github.com/u1and0/satrace
+```
+
+
+# Licence
+MIT
+
+Copyright (c) 2020 u1and0
+http://wisdommingle.com/
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.go
@@ -0,0 +1,320 @@
+/*
+satrace - SAtrace project's CLI tool
+Convert formatted text to Date + Data rows asynchronously
+
+Usage:
+```
+# No option
+# Dump txt to SAtrace format data
+$ satrace *.txt
+2019-8-29 22:23:47  -35   -39.4   -55   ...
+2019-8-29 23:34:56  -31   -42.4   -43   ...
+
+# Electric Energy converter
+# X axis as line number
+$ satrace elen -f 425-575 *.txt
+
+# Electric Energy converter
+# X axis as point read from first line configure
+$ satrace elen -F 42-46 *.txt
+
+# Peak search
+$ satrace peak *.txt
+```
+*/
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/mitchellh/cli"
+)
+
+const (
+	// VERSION info
+	VERSION = "v0.1.0"
+)
+
+var (
+	// version flag
+	showVersion bool
+	// field code
+	field arrayField
+	// usecol is column of using calculation
+	usecol int
+	// logger print to stdout
+	delim string
+	// debug mode
+	debug bool
+	// wg wait goroutine
+	wg sync.WaitGroup
+	// logger print to stdout
+	logger = log.New(os.Stdout, "", 0)
+)
+
+type (
+	// arrayField created so that multiple inputs can be accecpted
+	arrayField []string
+	// configMap is a first line of data
+	configMap map[string]string
+	// contentArray read from data
+	contentArray []float64
+	// OutRow is a output line
+	OutRow struct {
+		Filename   string
+		Datetime   string
+		Center     string
+		Fields     []float64
+		FieldRange arrayField // []string multiple flags
+		Debug      bool
+	}
+	// Command is a list of subcommand
+	Command interface {
+		// Convert formatted text to Date + Data rows asynchronously
+		Help() string
+		Elen(args []string) int
+	}
+	// ElenCommand command definition
+	// type Elen struct{}
+	ElenCommand struct{}
+)
+
+func main() {
+	// flag.BoolVar(&showVersion, "v", false, "Show version")
+	// flag.Var(&field, "f", "Field range such as -f 50-100")
+	// flag.IntVar(&usecol, "c", 1, "Column of using calculation")
+	// flag.StringVar(&delim, "d", "\t", "A character of separated delimeter")
+	// flag.BoolVar(&debug, "debug", false, "Debug mode")
+	// flag.Parse()
+	// if showVersion {
+	// 	fmt.Println("satrace version:", VERSION)
+	// 	return // Exit with version info
+	// }
+	//
+	c := cli.NewCLI("satrace", "0.1.0") // subcommand struct
+	c.Args = os.Args[1:]
+	// Subcommands register
+	c.Commands = map[string]cli.CommandFactory{
+		"elen": func() (cli.Command, error) {
+			return &ElenCommand{}, nil
+		},
+	}
+
+	// ElenCommand{}.writeBuffer(files)
+	exitCode, err := c.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute: %s\n", err.Error())
+	}
+	os.Exit(exitCode)
+}
+
+// Synopsis message of `satrace elen`
+func (e *ElenCommand) Synopsis() string {
+	return "elen Synopsis"
+}
+
+// Help message of `satrace elen`
+func (e *ElenCommand) Help() string {
+	return "elen help"
+}
+
+// Run print result of writeOutRow()
+func (e *ElenCommand) Run(args []string) int {
+	flags := flag.NewFlagSet("elen", flag.ContinueOnError)
+	flags.Var(&field, "f", "Field range such as -f 50-100")
+	flags.IntVar(&usecol, "c", 1, "Column of using calculation")
+	flags.BoolVar(&debug, "debug", false, "Debug mode")
+	if err := flags.Parse(args); err != nil {
+		return 1
+	}
+
+	o := OutRow{FieldRange: field, Debug: debug}
+	for _, filename := range flags.Args() {
+		// File not exist then next loop
+		if _, err := os.Stat(filename); err != nil {
+			continue
+		}
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			var err error
+			o, err = o.writeOutRow(f)
+			if err != nil {
+				panic(err)
+			}
+			logger.Println(o)
+		}(filename)
+	}
+	wg.Wait()
+	return 0
+}
+
+// writeOutRow return a line of processed content
+func (o OutRow) writeOutRow(s string) (OutRow, error) {
+	// var (
+	// 	config  configMap
+	// 	content contentArray
+	// 	m, n    int
+	// )
+	o.Filename = s
+	o.Datetime = parseDatetime(filepath.Base(s))
+	config, content, err := readTrace(s)
+	if err != nil {
+		return o, err
+	}
+	if o.Debug {
+		logger.Printf("[ CONFIG ]:%v\n", config)
+		logger.Printf("[ CONTENT ]:%v\n", content)
+		logger.Printf("[ FIELD ]:%v\n", o.FieldRange)
+	}
+	o.Center = config[":FREQ:CENT"]
+	for _, f := range o.FieldRange {
+		m, n, err := parseField(f)
+		if err != nil {
+			return o, err
+		}
+		mw := content.signalBand(m, n)
+		o.Fields = append(o.Fields, mw)
+	}
+	// Debug print format
+	if o.Debug {
+		logger.Printf("[ TYPE OUTROW ]%v\n", o)
+		// continue // print not standard output
+		return o, err
+	}
+	return o, err
+}
+
+// OutRow.String print as comma separated value
+func (o OutRow) String() string {
+	s := fmt.Sprintf("%s,%s,%s", // comma separated
+		o.Datetime,
+		o.Center,
+		strings.Join(func() (ss []string) {
+			for _, f := range o.Fields { // convert []float64=>[]string
+				// s := strconv.FormatFloat(f, 'f', -1, 64)
+				s := fmt.Sprintf("%e", f)
+				ss = append(ss, s)
+			}
+			return
+		}(), ","), // comma separated
+	)
+	return s
+}
+
+// signalBand convert mWatt then sum between band
+func (c contentArray) signalBand(m, n int) (mw float64) {
+	for i := m; i <= n; i++ {
+		mw += db2mw(c[i])
+	}
+	return
+}
+
+// parseConfig convert first line of data to config map
+func parseConfig(b []byte) configMap {
+	config := make(configMap)
+	sarray := bytes.Split(b, []byte(";"))
+	// snip # 20200627_180505 *RST & *CLS
+	// chomp last new line
+	sa := sarray[2 : len(sarray)-1]
+	for _, e := range sa {
+		kv := strings.Fields(string(e))
+		config[kv[0]] = strings.Join(kv[1:], " ")
+	}
+	return config
+}
+
+// parseFilename convert a filename as datetime (%Y-%m-%d %H:%M:%S) format
+func parseDatetime(s string) string {
+	return fmt.Sprintf("%s-%s-%s %s:%s:%s", // 2006-01-02 15:05:12
+		s[0:4], s[4:6], s[6:8], s[9:11], s[11:13], s[13:15])
+}
+
+// parseField convert -f option to 2 int pair
+func parseField(s string) (i0, i1 int, err error) {
+	if !strings.Contains(s, "-") {
+		err = errors.New("Error: Field flag -f " + s +
+			" not contains range \"-\", use int-int")
+		return
+	}
+	ss := strings.Split(s, "-")
+	i0, err = strconv.Atoi(ss[0])
+	i1, err = strconv.Atoi(ss[1])
+	if i0 > i1 {
+		err = fmt.Errorf("Error: Must be lower %d than %d", i0, i1)
+	}
+	return
+}
+
+// arrayField.String sets multiple -f flag
+func (i *arrayField) String() string {
+	// change this, this is just can example to satisfy the interface
+	return "my string representation"
+}
+
+// arrayField.Set sets multiple -f flag
+func (i *arrayField) Set(value string) error {
+	*i = append(*i, strings.TrimSpace(value))
+	return nil
+}
+
+// db2mw returns dB convert to mWatt
+func db2mw(db float64) float64 {
+	return math.Pow(10, db/10)
+}
+
+// readTrace read from a filename to `config` from first line,
+// `content` from no # line.
+func readTrace(filename string) (config configMap, content contentArray, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var (
+		line   []byte
+		isConf = true
+		f      float64
+	)
+	for {
+		line, _, err = reader.ReadLine()
+		if isConf { // First line is configure
+			config = parseConfig(line)
+			isConf = false
+			continue
+		}
+		if bytes.HasPrefix(line, []byte("#")) {
+			// Got "# <eof>" successful terminationthen
+			return
+		}
+		if err == io.EOF { // if EOF then finish func
+			err = nil
+			logger.Println("warning: data has not <eof>")
+			return // might not work because HasPrefix([]byte("#"))
+		}
+		if err != nil { // if error at ReadLine then finish func
+			return
+		}
+		// Trim Prefix/Surfix/Middle whitespace
+		bb := bytes.Fields(bytes.TrimSpace(line))
+		f, err = strconv.ParseFloat(string(bb[usecol]), 64)
+		if err != nil {
+			return
+		}
+		content = append(content, f)
+	}
+}
